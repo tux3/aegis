7a8a61dcb32522f4063994b39e9220d9ad5561fd
tux3 <barrdetwix@gmail.com>
2021-08-29 02:19:12 +0200

    Add cpu:x86 alias to request automatic module load
    Of course this won't auto-load on ARM, but that is out of scope!

--- mod.c
+++ mod.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/slab.h>
+#include <asm/cpufeatures.h>
+#include <asm/cpu_device_id.h>
 
 static void __cleanup(void)
 {
@@ -24,6 +26,12 @@ fail:
 
 module_init(init);
 module_exit(cleanup);
+
+static const struct x86_cpu_id any_cpu_id[] = {
+	X86_MATCH_FEATURE(X86_FEATURE_ANY, NULL),
+	{}
+};
+MODULE_DEVICE_TABLE(x86cpu, any_cpu_id);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("tux3 <barrdetwix@gmail.com>");
