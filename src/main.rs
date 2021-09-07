26ec9bbfc7c4133de7303f511f0888c79b4ac809
tux3 <barrdetwix@gmail.com>
2021-09-07 15:20:38 +0200

    Adjust log level to silence actix-server info

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> Result<()> {
         std::env::set_var("RUST_LIB_BACKTRACE", "1")
     }
     if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "info")
+        std::env::set_var("RUST_LOG", "info,actix_server=warn")
     }
     tracing_subscriber::fmt::fmt()
         .with_env_filter(EnvFilter::from_default_env())
