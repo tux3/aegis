70b8642484c9b1a0e45b388e86139d62fc13f4b9
tux3 <barrdetwix@gmail.com>
2021-09-09 01:30:42 +0200

    Preserve status code when wrapping actix errors in crate::error::Error

--- src/error.rs
+++ src/error.rs
@@ -1,44 +1,62 @@
+use actix_web::http::StatusCode;
 ///! Defines an newtype wrapper around anyhow::Error
 ///! Required because the orphan rule prevent us from having From<sqlx::Error> for actix_web::Error
 use actix_web::ResponseError;
 use anyhow::anyhow;
-use derive_more::Display;
-use std::fmt::Debug;
+use std::fmt::{Debug, Display, Formatter};
 
 /// Replaces anyhow::bail! in functions that must return a crate::error::Error
 macro_rules! bail {
     ($msg:literal $(,)?) => {
-        return Err(crate::error::Error(anyhow::anyhow!($msg)))
+        return Err(crate::error::Error::Anyhow(anyhow::anyhow!($msg)))
     };
     ($err:expr $(,)?) => ({
-        return Err(crate::error::Error(anyhow::anyhow!($err)))
+        return Err(crate::error::Error::Anyhow(anyhow::anyhow!($err)))
     });
     ($fmt:expr, $($arg:tt)*) => {
-        return Err(crate::error::Error(anyhow::anyhow!($fmt, $($arg)*)))
+        return Err(crate::error::Error::Anyhow(anyhow::anyhow!($fmt, $($arg)*)))
     };
 }
 pub(crate) use bail;
 
-#[derive(Debug, Display)]
-#[display(fmt = "{}", _0)]
-pub struct Error(pub anyhow::Error);
+#[derive(Debug)]
+pub enum Error {
+    Anyhow(anyhow::Error),
+    Actix(actix_web::Error),
+}
 
-impl ResponseError for Error {}
+impl ResponseError for Error {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            Self::Actix(e) => e.as_response_error().status_code(),
+            Self::Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Anyhow(e) => write!(f, "{}", e),
+            Self::Actix(e) => write!(f, "{}", e),
+        }
+    }
+}
 
 impl From<anyhow::Error> for Error {
     fn from(e: anyhow::Error) -> Self {
-        Self(e)
+        Self::Anyhow(e)
     }
 }
 
 impl From<actix_web::Error> for Error {
     fn from(e: actix_web::Error) -> Self {
-        Self(anyhow!("{}", e))
+        Self::Actix(e)
     }
 }
 
 impl From<sqlx::Error> for Error {
     fn from(e: sqlx::Error) -> Self {
-        Self(anyhow!("Database error: {}", e))
+        Self::Anyhow(anyhow!("Database error: {}", e))
     }
 }
