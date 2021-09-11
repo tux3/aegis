6338b6ed9fa77285efc01773bf7b4539e24729fa
tux3 <barrdetwix@gmail.com>
2021-09-12 00:34:30 +0200

    Load device status from server

--- app/src/main/java/net/alacrem/aegis/ui/DeviceSettingsActivity.kt
+++ app/src/main/java/net/alacrem/aegis/ui/DeviceSettingsActivity.kt
@@ -1,17 +1,23 @@
 package net.alacrem.aegis.ui
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.alacrem.aegis.R
 import net.alacrem.aegis.databinding.ActivityDeviceSettingsBinding
 import net.alacrem.aegis.ui.main.MainActivity
-import uniffi.client.RootKeys
 import androidx.core.app.NavUtils
 
 import android.view.MenuItem
-
-
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import net.alacrem.aegis.defaultClientConfig
+import uniffi.client.*
 
 
 @ExperimentalUnsignedTypes
@@ -19,6 +25,7 @@ class DeviceSettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDeviceSettingsBinding
     private lateinit var keys: RootKeys
     private lateinit var deviceName: String
+    private lateinit var client: AdminClientFfi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,15 +54,85 @@ class DeviceSettingsActivity : AppCompatActivity() {
             keys = maybeKeys
             deviceName = maybeDevName
         }
+        client = AdminClientFfi(defaultClientConfig(), keys)
 
         actionBar?.title = "$deviceName device settings"
         supportActionBar?.title = "$deviceName device settings"
 
-        // TODO: Actually load current status
-        // TODO: Update status in background when widget triggered
-
         binding = ActivityDeviceSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.lockBtn.setOnClickListener {
+            updateStatusSync(SetStatusArg(deviceName, vtLocked = true, sshLocked = true))
+        }
+        binding.unlockBtn.setOnClickListener {
+            updateStatusSync(SetStatusArg(deviceName, vtLocked = false, sshLocked = false))
+        }
+        refreshStatus()
+    }
+
+    private fun refreshStatus() {
+        updateStatusSync(SetStatusArg(deviceName, null, null))
+    }
+
+    private fun updateStatusSync(statusChange: SetStatusArg) {
+        disableUi()
+        binding.settingsLoading.visibility = View.VISIBLE
+        binding.settingsLoadingBg.visibility = View.VISIBLE
+        lifecycleScope.launch(Dispatchers.IO) {
+            val newStatus = try {
+                client.setStatus(statusChange)
+            } catch (e: FfiException) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(applicationContext, "Failed to set status", Toast.LENGTH_LONG).show()
+                    binding.settingsLoading.visibility = View.GONE
+                    binding.settingsLoadingBg.visibility = View.GONE
+                    enableUi()
+                }
+                return@launch
+            }
+            withContext(Dispatchers.Main) {
+                applyStatusReply(newStatus)
+            }
+        }
+    }
+
+    private fun disableUi() {
+        binding.settingsVlayout.isEnabled = false
+        binding.lockBtn.isEnabled = false
+        binding.unlockBtn.isEnabled = false
+        binding.vtLock.setOnCheckedChangeListener(null)
+        binding.vtLock.isEnabled = false
+        binding.sshLock.setOnCheckedChangeListener(null)
+        binding.sshLock.isEnabled = false
+    }
+
+    private fun enableUi() {
+        binding.lockBtn.isEnabled = true
+        binding.unlockBtn.isEnabled = true
+        binding.vtLock.setOnCheckedChangeListener { _, isChecked ->
+            updateStatusSync(SetStatusArg(deviceName, vtLocked = isChecked, null))
+        }
+        binding.vtLock.isEnabled = true
+        binding.sshLock.setOnCheckedChangeListener { _, isChecked ->
+            updateStatusSync(SetStatusArg(deviceName, null, sshLocked = isChecked))
+        }
+        binding.sshLock.isEnabled = true
+        binding.settingsVlayout.isEnabled = true
+    }
+
+    private fun applyStatusReply(status: StatusReply) {
+        setSwitch(binding.vtLock, status.vtLocked)
+        setSwitch(binding.sshLock, status.sshLocked)
+        enableUi()
+        binding.settingsLoading.visibility = View.GONE
+        binding.settingsLoadingBg.visibility = View.GONE
+        binding.settingsVlayout.isEnabled = true
+    }
+
+    private fun setSwitch(switch: SwitchCompat, state: Boolean) {
+        switch.isChecked = state
+        switch.jumpDrawablesToCurrentState()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
