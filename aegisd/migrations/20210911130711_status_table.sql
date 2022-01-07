05e4ab70f74a18123b8f301c022d1caab46bc477
tux3 <barrdetwix@gmail.com>
2022-01-07 23:05:33 +0100

    aegisd: CASCADE DELETE forein keys in device_status table

--- aegisd/migrations/20210911130711_status_table.sql
+++ aegisd/migrations/20210911130711_status_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE device_status(
-    dev_id integer REFERENCES device(id) NOT NULL,
+    dev_id integer REFERENCES device(id) ON DELETE CASCADE NOT NULL,
     updated_at timestamp NOT NULL,
     vt_locked boolean NOT NULL DEFAULT FALSE,
     ssh_locked boolean NOT NULL DEFAULT FALSE
