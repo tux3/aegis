9bba1ea1832548f6164a3cc18b181a103408ca78
tux3 <barrdetwix@gmail.com>
2022-01-27 23:10:11 +0100

    aegisk: Do VT switch manually, without requirring an attached TTY
    Previously we called an ioctl on get_current_tty(), but when the
    aegis usermode helper triggers lock_vt, it doesn't have a tty.
    Fortunately, the fg_console and vt_dont_switch are actually
    global state, and nothing that the TTY ioctl does actually requires
    a TTY here.
    So we can just do what the ioctl does internally, by resolving
    the right symbols with KLA and using them directly.

--- aegisk/lock.c
+++ aegisk/lock.c
@@ -9,43 +9,78 @@
 
 static const int LOCKED_VT_NUM = 25; // Arbitrary. Hope you don't use this VT!
 static int previous_vt;
-static int (*_vt_ioctl)(struct tty_struct *tty, unsigned int cmd,
-			unsigned long arg);
+
+static int (*_vt_waitactive)(int nr);
+static int (*_vc_allocate)(unsigned int curcons);
+static int (*_set_console)(int nr);
+static bool* _vt_dont_switch;
+
+static int aegis_vt_activate(int vt_num)
+{
+	int ret;
+
+	if (vt_num == 0 || vt_num > MAX_NR_CONSOLES)
+		return -ENXIO;
+	vt_num--;
+
+	console_lock();
+	ret = _vc_allocate(vt_num);
+	console_unlock();
+	if (ret)
+		return ret;
+	_set_console(vt_num);
+	return 0;
+}
+
+static int aegis_vt_wait_active(int vt_num)
+{
+	if (vt_num == 0 || vt_num > MAX_NR_CONSOLES)
+		return -ENXIO;
+	return _vt_waitactive(vt_num);
+}
+
+static int aegis_vt_lockswitch(bool lock)
+{
+	*_vt_dont_switch = lock;
+	return 0;
+}
 
 int aegisk_lock_vt(void)
 {
-	struct tty_struct *tty = get_current_tty();
-	if (!tty)
-		return -ENOTTY;
+	int ret;
 
 	console_lock();
 	previous_vt = fg_console;
 	console_unlock();
 
-	_vt_ioctl(tty, VT_ACTIVATE, LOCKED_VT_NUM + 1);
-	_vt_ioctl(tty, VT_WAITACTIVE, LOCKED_VT_NUM + 1);
-	_vt_ioctl(tty, VT_LOCKSWITCH, 0);
+	if ((ret = aegis_vt_activate(LOCKED_VT_NUM + 1)))
+		return ret;
+	aegis_vt_wait_active(LOCKED_VT_NUM + 1);
+	aegis_vt_lockswitch(true);
 
-	tty_kref_put(tty);
 	return 0;
 }
 
 int aegisk_unlock_vt(void)
 {
-	struct tty_struct *tty = get_current_tty();
-	if (!tty)
-		return -ENOTTY;
-
-	_vt_ioctl(tty, VT_UNLOCKSWITCH, 0);
-	_vt_ioctl(tty, VT_ACTIVATE, previous_vt + 1);
-	tty_kref_put(tty);
+	aegis_vt_lockswitch(false);
+	aegis_vt_activate(previous_vt + 1);
 	return 0;
 }
 
 int init_locking(void)
 {
-	_vt_ioctl = (void *)module_kallsyms_lookup_name("vt_ioctl");
-	if (!_vt_ioctl)
+	_vt_dont_switch = (void *)module_kallsyms_lookup_name("vt_dont_switch");
+	if (!_vt_dont_switch)
+		return -ENOENT;
+	_vt_waitactive = (void *)module_kallsyms_lookup_name("vt_waitactive");
+	if (!_vt_waitactive)
+		return -ENOENT;
+	_vc_allocate = (void *)module_kallsyms_lookup_name("vc_allocate");
+	if (!_vc_allocate)
+		return -ENOENT;
+	_set_console = (void *)module_kallsyms_lookup_name("set_console");
+	if (!_set_console)
 		return -ENOENT;
 	return 0;
 }
