9d9fb0439200d87020444628ac59867b7bc36d17
tux3 <barrdetwix@gmail.com>
2022-02-03 20:00:53 +0100

    aegisc: Sync before reboot/poweroff

--- aegisc/src/power.rs
+++ aegisc/src/power.rs
@@ -1,9 +1,11 @@
 use crate::run_as::run_as_root;
 use aegislib::command::server::PowerCommand;
 use nix::sys::reboot::{reboot, RebootMode};
+use nix::unistd::sync;
 use tracing::error;
 
 pub async fn apply_command(cmd: PowerCommand) {
+    sync();
     let _ = match cmd {
         PowerCommand::Poweroff => reboot(RebootMode::RB_POWER_OFF),
         PowerCommand::Reboot => reboot(RebootMode::RB_AUTOBOOT),
